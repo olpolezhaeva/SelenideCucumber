fd1f97a9259f185a03a3405eef9ef86e702ac01d
olpolezhaeva <olpolezhaeva2014@gmail.com>
2022-12-22 16:39:23 +0300

    initial commit

--- src/test/java/model/MainPage.java
+++ src/test/java/model/MainPage.java
@@ -1,70 +1,10 @@
 package model;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends BasePage{
 
-    @FindBy(id = "user-name")
-    private WebElement userNameField;
-
-    @FindBy(id = "password")
-    private WebElement passwordField;
-
-    @FindBy(id = "login-button")
-    private WebElement loginButton;
-
-    @FindBy(css = ".login_logo")
-    private WebElement loginLogo;
-
-    @FindBy(xpath = "//h3")
-    private WebElement errorMessage;
-
-    @FindBy(css = ".bot_column")
-    private WebElement picture;
-
     public MainPage(WebDriver driver) {
         super(driver);
-    }
-
-    public String getTextMessage(){
-
-        return errorMessage.getText();
-    }
-
-    public ProductsPage clickLoginButton(){
-        loginButton.click();
-        return new ProductsPage(getDriver());
-    }
-
-    public MainPage fillUserNameField(String value){
-        userNameField.sendKeys(value);
-        return this;
-    }
-
-    public MainPage fillPasswordField(String value){
-        passwordField.sendKeys(value);
-        return this;
-    }
-
-    public boolean isLogoDisplayed() {
-
-        return loginLogo.isEnabled();
-    }
-
-    public boolean isPictureDisplayed() {
-
-        return picture.isDisplayed();
-    }
-
-    public boolean isUserNameFieldDisplayed() {
-
-        return userNameField.isDisplayed();
-    }
-
-    public boolean isPasswordFieldDisplayed() {
-
-        return passwordField.isDisplayed();
     }
 }
